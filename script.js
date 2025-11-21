9e846d0ca2cc03af46255dd244f73be53e33b51c
obengmichael <obenowusumichael@gmail.com>
2025-11-21 07:59:11 -0800

    edited the css and html

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('resultscalculator').addEventListener('submit',(event) => {
+    event.preventDefault();
+
+let science = document.getElementById('science').value.trim();
+let mathematics = document.getElementById('mathematics').value.trim();
+let english = document.getElementById( 'english').value.trim();
+let socialstudies = document.getElementById('socialstudies').value.trim();
+let french = document.getElementById('french').value.trim();
+let  economics = document.getElementById('economics').value.trim();
+
+let message = document.getElementById('message')
+})
+
+
+
+
+
+let btn1 = document.getElementById();
+
+btn1.addEventListener('total score', () => {
+    
+    btn1.textContent = 'calculate marks';
+    
+});
